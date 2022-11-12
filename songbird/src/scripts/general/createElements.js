60fdb18ed76d76e474525d86082ce032a7d0b0ef
Nikita <nikitakemper555@gmail.com>
2022-11-12 23:23:40 +0300

    feat: add createElement and shuffle function

---
+++ songbird/src/scripts/general/createElements.js
@@ -0,0 +1,13 @@
+export default function createElement(tagName, defaultClassName) {
+    if (!tagName)
+        return null;
+    const element = document.createElement(tagName);
+
+    if (defaultClassName) {
+        defaultClassName.split(' ').forEach(className => {
+            element.classList.add(className);
+        })
+    }
+        
+    return element;
+}
---
+++ songbird/src/scripts/general/shuffle.js
@@ -0,0 +1,5 @@
+export function shuffle(arr) {
+    return arr.map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+}
