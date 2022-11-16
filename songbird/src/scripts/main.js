bca2d9aefd670cdcbd4a9ead27e1f8cb7d2ce5f0
Nikita <nikitakemper555@gmail.com>
2022-11-17 01:20:41 +0300

    feat: add header on the main page

--- songbird/src/scripts/main.js
+++ songbird/src/scripts/main.js
@@ -1,9 +1,4 @@
 import '../styles/main.scss';
 
-let el = document.createElement('div');
-el.textContent = 'Element from JS';
-el.className = 'style';
-document.body.appendChild(el);
-
 console.log(1);
 
